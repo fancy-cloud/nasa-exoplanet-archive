6646f8b1e120d0ec683a3615edfbdc8b60e887c6
fancy-cloud <>
2021-04-05 23:10:22 +0300

    added templates

--- nasa_exoplanet_archive/main/views.py
+++ nasa_exoplanet_archive/main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ExoplanetsData
 
 # Create your views here.
 def index(response):
-	return HttpResponse("<h1>Init</h1>")
+	exoplanets_list = ExoplanetsData.objects.all()
+	return render(response, "main/index.html", {"exoplanets_list": exoplanets_list})
