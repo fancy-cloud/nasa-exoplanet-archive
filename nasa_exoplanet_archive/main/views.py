004d24e2e43fe1e74c608e7359923ecb932be18c
fancy-cloud <>
2021-04-08 01:46:57 +0300

    ugly html with forms

--- nasa_exoplanet_archive/main/views.py
+++ nasa_exoplanet_archive/main/views.py
@@ -5,4 +5,20 @@ from .models import ExoplanetsData
 # Create your views here.
 def index(response):
 	exoplanets_list = ExoplanetsData.objects.all()
+
+	if response.method == "POST":
+		print(response.POST)
+		if response.POST.get("sort-by-year-asc"):
+			exoplanets_list = ExoplanetsData.objects.order_by("discovery_year")
+		elif response.POST.get("sort-by-year-desc"):
+			exoplanets_list = ExoplanetsData.objects.order_by("-discovery_year")
+		elif response.POST.get("sort-by-light-years-asc"):
+			exoplanets_list = ExoplanetsData.objects.order_by("light_years")
+		elif response.POST.get("sort-by-light-years-desc"):
+			exoplanets_list = ExoplanetsData.objects.order_by("-light_years")
+		elif response.POST.get("sort-by-mass-asc"):
+			exoplanets_list = ExoplanetsData.objects.order_by("planet_mass")
+		elif response.POST.get("sort-by-mass-desc"):
+			exoplanets_list = ExoplanetsData.objects.order_by("-planet_mass")
+			
 	return render(response, "main/index.html", {"exoplanets_list": exoplanets_list})
